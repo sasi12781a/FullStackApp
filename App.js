36099c5a01f07983fac1e0c826fd42c21ff06cdb
sasi12781a <sasi.chimmili@xcubelabs.com>
2022-12-09 11:42:11 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  Image,
+  ActivityIndicator,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';    
+
+
+const App=()=>{
+  return(
+    <View style={{display:'flex',justifyContent:"center",alignItems:'center',height:'100%'}}>
+      <Text>
+        Hi sasi
+      </Text>
+    </View>
+  )
+}
+
+export default App;
