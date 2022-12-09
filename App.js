91c6ed8a3d1e41ac9f6c401e1eece0b0c5ab26b8
sasi12781a <sasi.chimmili@xcubelabs.com>
2022-12-09 13:08:22 +0530

    first commit

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import {
   Text,
   View,
@@ -10,11 +10,23 @@ import {
 
 
 const App=()=>{
+  const [mode,setMode] = useState(false);
+  function Mode(){
+    setMode(!mode)
+  }
   return(
-    <View style={{display:'flex',justifyContent:"center",alignItems:'center',height:'100%'}}>
-      <Text>
-        Hi sasi
-      </Text>
+    <View style={{display:'flex',height:'100%',backgroundColor:mode?"#000":'#fff',justifyContent:'center',alignItems:'center'}}>
+      
+      <View style={{flex:1,justifyContent:'flex-start',alignSelf:"flex-end"}}>
+        <TouchableOpacity  onPress={Mode}>
+          <Text style={{color:mode?"#fff":'#000',margin:10}}>DarkMode</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{flex:1}}>
+        <Text style={{color:mode?"#fff":'#000',justifyContent:'center'}}>
+          Hi sasi
+        </Text>
+      </View>  
     </View>
   )
 }
